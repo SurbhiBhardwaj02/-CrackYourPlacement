d641676e14a9358ea447cda942e955b057dfea9d
SurbhiBhardwaj <92331117+SurbhiBhardwaj02@users.noreply.github.com>
2023-07-03 00:07:58 +0530

    Add files via upload

---
+++ Arrays/BestTimetoBuyandSellStock.java
@@ -0,0 +1,20 @@
+package Arrayss;
+
+public class BestTimetoBuyandSellStock {
+    public int maxProfit(int[] prices) {
+        int minprice=prices[0];
+        int profit=0;
+        for(int i=1;i<prices.length;i++)
+        {
+            if(prices[i]<minprice)
+            {
+                minprice=prices[i];
+            }
+            if(prices[i]-minprice>profit)
+            {
+                profit = prices[i]-minprice;
+            }
+        }
+        return profit;
+    }
+}
