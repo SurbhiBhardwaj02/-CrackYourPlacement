2a006e71b27c0003fed59d5150250e314af07be0
SurbhiBhardwaj <92331117+SurbhiBhardwaj02@users.noreply.github.com>
2023-07-04 22:52:22 +0530

    Add files via upload

---
+++ Heap/kthSmallestInSortedArr.java
@@ -0,0 +1,19 @@
+package Heap;
+
+import java.util.PriorityQueue;
+
+public class kthSmallestInSortedArr {
+    public int kthSmallest(int[][] matrix, int k) {
+        PriorityQueue<Integer> pq= new PriorityQueue<>();
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                pq.add(matrix[i][j]);
+            }
+
+        }
+        for(int i=0;i<k-1;i++){
+            pq.poll();
+        }
+        return pq.peek();
+    }
+}
