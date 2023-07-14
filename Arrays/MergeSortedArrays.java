328c071550f8bb96f8f8726e3219b29a5aa40d0c
SurbhiBhardwaj <92331117+SurbhiBhardwaj02@users.noreply.github.com>
2023-07-14 22:42:09 +0530

    Add files via upload

---
+++ Arrays/MergeSortedArrays.java
@@ -0,0 +1,23 @@
+package Arrayss;
+
+public class MergeSortedArrays {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i1 = m - 1; // pointer for last element in nums1
+        int i2 = n - 1; // pointer for last element in nums2
+
+        // Loop through the arrays, from last element to first
+        while (i1 >= 0 && i2 >= 0) {
+            if (nums1[i1] >= nums2[i2]) {
+                nums1[i1 + i2 + 1] = nums1[i1--];
+            } else {
+                nums1[i1 + i2 + 1] = nums2[i2--];
+            }
+        }
+
+        // Copy in the remaining elements
+        while (i2 >= 0) {
+            nums1[i2] = nums2[i2--];
+        }
+
+    }
+}
